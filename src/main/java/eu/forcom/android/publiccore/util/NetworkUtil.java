484ebacab8e446b76707b2c3a8d8617e7c022f45
Bartosz Kosarzycki <bkosarzycki@forcom.com.pl>
2015-03-13 14:31:49 +0100

    NetworkUtil

---
+++ src/main/java/eu/forcom/android/publiccore/util/NetworkUtil.java
@@ -0,0 +1,35 @@
+package eu.forcom.android.publiccore.util;
+
+import java.net.Inet4Address;
+import java.net.InetAddress;
+import java.net.NetworkInterface;
+import java.net.SocketException;
+import java.util.Enumeration;
+
+public class NetworkUtil {
+
+    /**
+     * Gets the primary IP address of the device
+     *
+     * http://stackoverflow.com/questions/6064510/how-to-get-ip-address-of-the-device
+     *
+     * @return
+     */
+    public static String getLocalIpAddress() {
+        try {
+            for (Enumeration<NetworkInterface> en = NetworkInterface.getNetworkInterfaces(); en.hasMoreElements();) {
+                NetworkInterface intf = en.nextElement();
+                for (Enumeration<InetAddress> enumIpAddr = intf.getInetAddresses(); enumIpAddr.hasMoreElements();) {
+                    InetAddress inetAddress = enumIpAddr.nextElement();
+                    if (!inetAddress.isLoopbackAddress() && inetAddress instanceof Inet4Address) {
+                        return inetAddress.getHostAddress();
+                    }
+                }
+            }
+        } catch (SocketException ex) {
+            ex.printStackTrace();
+        }
+
+        return null;
+    }
+}
